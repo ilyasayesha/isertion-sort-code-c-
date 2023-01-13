9c7347ebfe385058f3dbc2e141ce276291ce10d4
ilyasayesha <121997444+ilyasayesha@users.noreply.github.com>
2023-01-13 16:55:12 +0500

    Add files via upload

---
+++ insertion sort in array by assending.cpp
@@ -0,0 +1,32 @@
+//insertion sort by assending
+#include <iostream>
+using namespace std;
+int main (){
+	int n,j,current;
+	cout<<"\n Enter the size of array:"<<endl;
+	cin>>n;
+	int arr[n];
+	cout<<"Enter unsorted array:"<<endl;
+	for(int i=0;i<n;i++)
+	{
+		cin>>arr[i];	}
+	for(int i=1;i<n;i++){
+		current=arr[i];
+		j=(i-1);	
+		while(current<arr[j] && j>=0 ){
+			arr[j+1]=arr[j];
+			j--;
+		}
+		arr[j+1]=current;
+		cout<<"Insertion the sort step by setp:"<<endl;
+		for(int k=0;k<n;k++){
+					cout<<arr[k]<<",";
+				}
+				cout<<endl;
+		}
+		cout<<"Display the  final insertion sort:"<<endl;
+		for(int i=0;i<n;i++){
+		cout<<arr[i]<<" ,";	
+		}
+		return 0;
+	}
